3c2987e80238e7fe2cb88bae2d3be47a3cbd3193
Darren Derks <leederksjr@gmail.com>
2022-02-17 02:58:19 -0700

    Created a Node.js server backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <header className="row">
           <div>
             <a className="brand" href="/">
-              amazona
+              Darazon
             </a>
           </div>
           <div>
